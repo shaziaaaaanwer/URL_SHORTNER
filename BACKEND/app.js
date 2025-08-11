afa4c95948383488ab827fab40d9643f8bd5f5ca
Shazia Anwer <141482915+shaziaaaaanwer@users.noreply.github.com>
2025-08-12 00:34:09 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://url-shortner-frontend-ruby.vercel.app/"], // your React app
+    origin: ["https://url-shortner-frontend-ruby.vercel.app"], // your React app
     methods: ["GET", "POST"],
     credentials: true, // 👈 this allows cookies to be sent
   })
