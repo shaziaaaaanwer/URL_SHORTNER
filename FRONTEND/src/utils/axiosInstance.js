4a5c090c03589be2f2d1353a8a21f7358f84353b
Shazia Anwer <141482915+shaziaaaaanwer@users.noreply.github.com>
2025-08-12 01:30:54 +0530

    Update axiosInstance.js

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
     baseURL:"https://url-shortner-api-beryl.vercel.app/",
-    timeout:10000, //10s
+    timeout:20000, //10s
     withCredentials:true
 })
 
